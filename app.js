09622d6ac0f6d85666e48dad3bbee4624eb0a4a6
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-09-22 18:34:49 +0530

    commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let direction = 'right';
 let gameOver = false;
 
 let foodCells = generateRandomFood();
+let score = 0;
 
 document.addEventListener('keydown' , (event)=>{
     if(event.key === 'ArrowUp'){direction = "up"}
@@ -20,6 +21,9 @@ document.addEventListener('keydown' , (event)=>{
 function draw(){
     if(gameOver === true){
         clearInterval(id);
+        ctx.fillStyle = "red"
+        ctx.font = "50px sans-serif"
+        ctx.fillText('GAME OVER !!' , 430,400)
         return;
     }
     // eraser
@@ -28,10 +32,16 @@ function draw(){
     for(let cell of snakeCells){
         ctx.fillStyle = "brown"
         ctx.fillRect(cell[0],cell[1],cellSize,cellSize )
+        ctx.strokeStyle = "golden"
+        ctx.strokeRect(cell[0],cell[1],cellSize,cellSize )
     }
     // draw food
     ctx.fillStyle = "orange"
     ctx.fillRect(foodCells[0] , foodCells[1] , cellSize , cellSize)
+    // draw score
+    ctx.fillStyle = "white"
+    ctx.font = "20px monospace"
+    ctx.fillText(`Score: ${score}` , 20,30)
 }
 
 function update(){ 
@@ -42,25 +52,30 @@ function update(){
     if(direction ==='up'){
         newHeadX = headX;
         newHeadY = headY - cellSize;
-        if(newHeadY < 0){gameOver = true}
+        if(newHeadY < 0 || ex(newHeadX , newHeadY)){gameOver = true}
     }
     else if(direction ==='down'){
         newHeadX = headX;
         newHeadY = headY + cellSize;
-        if(newHeadY === boardHeight){gameOver=true}
+        if(newHeadY === boardHeight || ex(newHeadX , newHeadY)){gameOver=true}
     }
     else if(direction ==='left'){
         newHeadX = headX - cellSize;
         newHeadY = headY;
-        if(newHeadX <0){gameOver = true}
+        if(newHeadX <0 || ex(newHeadX , newHeadY)){gameOver = true}
     }
     else if(direction ==='right'){
         newHeadX = headX + cellSize;
         newHeadY = headY;
-        if(newHeadX === boardWidth){gameOver=true}
+        if(newHeadX === boardWidth || ex(newHeadX , newHeadY)){gameOver=true}
     }
     snakeCells.push([newHeadX , newHeadY]);
-    snakeCells.shift()
+    if(newHeadX === foodCells[0] && newHeadY === foodCells[1]){
+        foodCells = generateRandomFood()
+        score+=1;
+    }else{
+        snakeCells.shift()
+    }
 }
 
 
@@ -71,7 +86,16 @@ function generateRandomFood(){
     ]
 }
 
+function ex(newHeadX , newHeadY){
+    for(let item of snakeCells){
+        if(item[0] === newHeadX && item[1] === newHeadY){
+            return true;
+        }
+    }
+    return false;
+}
+
 let id = setInterval(function(){
     update()
     draw()
-} , 100)
+} , 150)
